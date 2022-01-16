d22d7c1af1be130e2a9161876470cea989a9a0ae
Jonas Mx <jonas.mahiques@gmail.com>
2022-01-16 18:42:09 +0100

    create class for iterable utils

--- lib/utils/iterable.dart
+++ lib/utils/iterable.dart
@@ -1,13 +1,15 @@
-Iterable<T> addBetweenEach<T>(Iterable<T> items, { T? element }) {
-  List<T> list = [];
+class IterableUtils {
+  static Iterable<T> addBetweenEach<T>(Iterable<T> items, { T? element }) {
+    List<T> list = [];
 
-  for (var i = 0; i < items.length; i++) {
-    list.add(items.elementAt(i));
+    for (var i = 0; i < items.length; i++) {
+      list.add(items.elementAt(i));
 
-    if (element != null && i < items.length-1) {
-      list.add(element);
+      if (element != null && i < items.length-1) {
+        list.add(element);
+      }
     }
-  }
 
-  return list;
+    return list;
+  }
 }
