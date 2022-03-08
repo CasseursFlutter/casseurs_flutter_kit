185b6f1d79f0f9d0813a09f99c2f3677dbc87064
Jonas Mx <jonas.mahiques@gmail.com>
2022-03-08 11:59:41 +0100

    improve image decoding

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,19 +1,18 @@
 import 'dart:io';
 import 'dart:isolate';
 import 'dart:typed_data';
+import 'dart:ui';
 import 'package:image/image.dart' as ImageUtils;
 
 class _DecodeParam {
-  final String filePath;
+  final String filePath;  
   final SendPort sendPort;
-  final int width;
-  final int height;
+  Size? size;
 
   _DecodeParam({
     required this.filePath,
     required this.sendPort,
-    required this.width,
-    required this.height
+    this.size,    
   });
 }
 
@@ -23,39 +22,42 @@ void _decodeIsolate(_DecodeParam param) async {
     Uint8List? encodedThumb;
 
     if (image != null) {
-      final sourceHeight = image.height;
-      final sourceWidth = image.width;
-      final sourceRatio = sourceWidth/sourceHeight;
+      if (param.size != null ) {
+        final sourceHeight = image.height;
+        final sourceWidth = image.width;
+        final sourceRatio = sourceWidth/sourceHeight;
 
-      final targetHeight = param.height;
-      final targetWidth = param.width;
-      final targetRatio = targetWidth/targetHeight;
+        final targetHeight = param.size!.height.round();
+        final targetWidth = param.size!.width.round();
+        final targetRatio = targetWidth/targetHeight;
 
-      
-      if (targetRatio > sourceRatio) {
-        final height = sourceWidth~/targetRatio;
-        image = ImageUtils.copyCrop(image, 0, (sourceHeight-height)~/2, sourceWidth, height);          
-      } else if (targetRatio < sourceRatio) {
-        final width = (sourceHeight*targetRatio).round();
-        image = ImageUtils.copyCrop(image, (sourceWidth-width)~/2, 0, width, sourceHeight);          
-      }
+        if (targetRatio > sourceRatio) {
+          final height = sourceWidth~/targetRatio;
+          image = ImageUtils.copyCrop(image, 0, (sourceHeight-height)~/2, sourceWidth, height);          
+        } else if (targetRatio < sourceRatio) {
+          final width = (sourceHeight*targetRatio).round();
+          image = ImageUtils.copyCrop(image, (sourceWidth-width)~/2, 0, width, sourceHeight);          
+        }
 
-      final thumb = ImageUtils.copyResize(image, width: targetWidth, height: targetHeight);
-      encodedThumb = Uint8List.fromList(ImageUtils.encodeJpg(thumb));
+        final thumb = ImageUtils.copyResize(image, width: targetWidth, height: targetHeight);
+        encodedThumb = Uint8List.fromList(ImageUtils.encodeJpg(thumb));
+      } else {
+        encodedThumb = Uint8List.fromList(ImageUtils.encodeJpg(image));
+      }      
     }
 
     param.sendPort.send(encodedThumb);
   }
 
 class ImagesUtils {  
-  static Future<Uint8List?> decodeAndResize(String filePath,  { int width = 300, int height = 300 }) async {
+  
+  static Future<Uint8List?> decodeAndResize(String filePath,  { Size? size }) async {
     final receivePort = ReceivePort();
     
     await Isolate.spawn(_decodeIsolate, _DecodeParam(
       filePath: filePath, 
       sendPort: receivePort.sendPort,
-      width: width,
-      height: height
+      size: size
     ));
 
     final image = await receivePort.first as Uint8List?;
